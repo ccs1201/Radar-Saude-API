af7b7c3b9fbf1774b3d043d9b35c644c87a752b2
Cleber Souza <ccs1201@gmail.com>
2022-06-03 18:18:57 -0300

    Test case PersonRepositoryTest implementado.

--- src/test/java/br/com/css/radarsaude/domain/repository/PersonRepositoryTest.java
+++ src/test/java/br/com/css/radarsaude/domain/repository/PersonRepositoryTest.java
@@ -29,23 +29,47 @@ class PersonRepositoryTest {
                 .name("Test Person")
                 .phoneNumber("48123456789")
                 .build();
+        savePerson();
     }
 
     @AfterAll
-    @Test
     @DisplayName("Tenta excluir a Person usada nos testes.")
     void deletePerson() {
+        repository.deleteById(person.getId());
 
+        assertTrue(repository.findById(person.getId()).isEmpty());
     }
 
     @Test
-    @DisplayName("Testa salvar um Person válido!")
+    @DisplayName("Tenta salvar um Person")
     void savePerson() {
         Person persisted = repository.save(person);
 
         assertEquals(persisted.getId(), person.getId());
 
     }
+
+    @Test
+    @DisplayName("Alterando a name da Person para novo teste")
+    void updatePerson(){
+
+        Person personFromBD = repository.findAll().get(0);
+        personFromBD.setName("novo teste");
+        long id = personFromBD.getId();
+
+        Person updated = repository.save(personFromBD);
+
+        assertEquals("novo teste", updated.getName());
+        assertEquals(id, updated.getId());
+        assertTrue(updated.getCreationDate().isBefore(updated.getLastUpdateDate()));
+    }
+
+    @Test
+    @DisplayName("Buscando Person pelo ID")
+    void findById(){
+        assertTrue(repository.findById(person.getId()).isPresent());
+    }
+
 
 
 }
